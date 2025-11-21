780b519822bdf27ac3cd64387ce6d90a0b0bdb04
K10-K10 <keito.tottori@gmail.com>
2025-11-21 15:37:10 +0900

    update make method

--- make.cpp
+++ make.cpp
@@ -8,104 +8,162 @@
 int x, h;
 std::random_device rd;
 std::mt19937 gen(rd());
-std::uniform_int_distribution<> forward(0, 3);
+std::uniform_int_distribution<> fw(0, 12);
+std::stack<std::pair<int, int>> cw;
 
-int wall(std::vector<std::vector<char>> &maze, int i, int j) {
-  int rand_f = forward(gen);
-  switch (rand_f) {
-  case 0:
-    if (i - 2 >= 0 && maze[i - 2][j] == ' ') {
-      maze[i - 1][j] = '#';
-      maze[i - 2][j] = '#';
-      wall(maze, i - 2, j);
-    } else {
-      maze[i - 1][j] = '#';
-      return 0;
+int wall(std::vector<std::vector<char>> &maze)
+{
+  int i = cw.top().first;
+  int j = cw.top().second;
+  if (maze[i][j] == '#')
+  {
+    cw.pop();
+    return 0;
+  }
+  cw.pop();
+  int cnt = 0;
+  bool wallFlag = false;
+  std::vector<int> forward;
+  maze[i][j] = '-';
+  while (!wallFlag)
+  {
+    cnt = 0;
+    if (maze[i - 2][j] != '-')
+    {
+      cnt++;
+      forward.push_back(0);
     }
-    break;
-
-  case 1:
-    if (j + 2 <= h && maze[i][j + 2] == ' ') {
-      maze[i][j + 1] = '#';
-      maze[i][j + 2] = '#';
-      wall(maze, i, j + 2);
-    } else {
-      maze[i][j + 1] = '#';
-      return 0;
+    if (maze[i][j + 2] != '-')
+    {
+      cnt++;
+      forward.push_back(1);
     }
-    break;
-
-  case 2:
-    if (i + 2 <= x && maze[i + 2][j] == ' ') {
-      maze[i + 1][j] = '#';
-      maze[i + 2][j] = '#';
-      wall(maze, i + 2, j);
-    } else {
-      maze[i + 1][j] = '#';
-      return 0;
+    if (maze[i + 2][j] != '-')
+    {
+      cnt++;
+      forward.push_back(2);
     }
-    break;
-
-  case 3:
-    if (j - 2 >= 0 && maze[i][j - 2] == ' ') {
-      maze[i][j - 1] = '#';
-      maze[i][j - 2] = '#';
-      wall(maze, i, j - 2);
-    } else {
-      maze[i][j - 1] = '#';
-      return 0;
+    if (maze[i][j - 2] != '-')
+    {
+      cnt++;
+      forward.push_back(3);
     }
-    break;
+    int f = forward[fw(gen) % cnt];
 
-  default:
-    break;
+    switch (f)
+    {
+    case 0:
+      maze[i - 1][j] = '-';
+      if (maze[i - 2][j] == '#')
+      {
+        wallFlag = true;
+      }
+      else
+      {
+        maze[i - 2][j] = '-';
+        i -= 2;
+      }
+      break;
+    case 1:
+      maze[i][j + 1] = '-';
+      if (maze[i][j + 2] == '#')
+      {
+        wallFlag = true;
+      }
+      else
+      {
+        maze[i][j + 2] = '-';
+        j += 2;
+      }
+      break;
+    case 2:
+      maze[i + 1][j] = '-';
+      if (maze[i + 2][j] == '#')
+        wallFlag = true;
+      else
+      {
+        maze[i + 2][j] = '-';
+        i += 2;
+      }
+      break;
+    case 3:
+      maze[i][j - 1] = '-';
+      if (maze[i][j - 2] == '#')
+        wallFlag = true;
+      else
+      {
+        maze[i][j - 2] = '-';
+        j -= 2;
+      }
+      break;
+    default:
+      return -1;
+      break;
+    }
   }
   return 0;
 }
 
-int main() {
+int main()
+{
   std::cout << "maze size(x h):";
   std::cin >> x >> h;
-  if (x < 5 || h < 5 || x % 2 == 0 || h % 2 == 0) {
+  if (x < 5 || h < 5 || x % 2 == 0 || h % 2 == 0)
+  {
     std::cerr << "maze size is odd numbers greater than or equal to five"
               << std::endl;
-    return 0;
+    return -1;
   }
   std::vector<std::vector<char>> maze(x, std::vector<char>(h, ' '));
-  std::vector<std::pair<int, int>> cw;
+  std::vector<std::pair<int, int>> plist;
+  int cnt = 0;
   {
     std::pair<int, int> ax;
-    for (int i = 0; i < x; i++) { // initialize field
-      for (int j = 0; j < h; j++) {
-        if (i == 0 || i == x - 1 || j == 0 || j == h - 1)
-          maze[i][j] = '#';
+    for (int i = 0; i < x; i++)
+    {
+      for (int j = 0; j < h; j++)
+      {
         if (i == 1 && j == 1)
           maze[i][j] = 'S';
         if (i == x - 2 && j == h - 2)
           maze[i][j] = 'G';
-        else if (i % 2 == 0 && j % 2 == 0) {
-          ax.first = i;
-          ax.second = j;
-          cw.push_back(ax);
+        if (i == 0 || i == x - 1 || j == 0 || j == h - 1)
+          maze[i][j] = '#';
+        else if (i % 2 == 0 && j % 2 == 0)
+        {
+          plist.push_back({i, j});
+          cnt++;
         }
       }
     }
   }
+  std::uniform_int_distribution<> pl(0, --cnt);
+  int rand = 0;
+  while (plist.size() > 0)
   {
-    while (cw.size() > 0) {
-      std::uniform_int_distribution<> dist(0, cw.size() - 1);
-      int rand = dist(gen) % cw.size();
-      if (maze[cw[rand].first][cw[rand].second] != '#') {
-        maze[cw[rand].first][cw[rand].second] = '#';
-        wall(maze, cw[rand].first, cw[rand].second);
+    rand = pl(gen);
+    cw.push(plist[rand % plist.size()]);
+    plist.erase(plist.begin() + rand % plist.size());
+  }
+  while (cw.size() > 0)
+  {
+    wall(maze);
+    for (int i = 0; i < x; i++)
+    {
+      for (int j = 0; j < h; j++)
+      {
+        if (maze[i][j] == '-')
+          maze[i][j] = '#';
       }
-      cw.erase(cw.begin() + rand);
     }
   }
-  for (int i = 0; i < x; i++) {
-    for (int j = 0; j < h; j++) {
+  for (int i = 0; i < x; i++)
+  {
+    for (int j = 0; j < h; j++)
+    {
       std::cout << maze[i][j];
     }
     std::cout << std::endl;
   }
+  return 0;
 }
